61195de67fb4345ca1d8e9d615fe62df560d39b6
Roxana Camelia Sabo <Sabo.Gh.Roxana@student.utcluj.ro>
2021-12-09 18:23:47 +0200

    Add project files.

---
+++ Sabo_Roxana_Lab10/ListPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sabo_Roxana_Lab10.Models;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Sabo_Roxana_Lab10
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListPage : ContentPage
+    {
+        public ListPage()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var slist = (ShopList)BindingContext;
+            slist.Date = DateTime.UtcNow;
+            await App.Database.SaveShopListAsync(slist);
+            await Navigation.PopAsync();
+        }
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var slist = (ShopList)BindingContext;
+            await App.Database.DeleteShopListAsync(slist);
+            await Navigation.PopAsync();
+        }
+    }
+}
